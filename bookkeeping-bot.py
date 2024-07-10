4633e6cc6fcd263b1fa30000f8add8cf9c29e7c2
yrc40 <yiruchen8266.mg10@nycu.edu.tw>
2024-07-10 14:33:54 -0400

    add fuction of query

--- bookkeeping-bot.py
+++ bookkeeping-bot.py
@@ -15,10 +15,10 @@ def get_num_of_people(message):
     global n
     if message.text.isdigit():
         n = int(message.text)
-        bot.reply_to(message, 
+        bot.reply_to(message,
                      f"OK, {n} people.\nPlease enter users' names in a line and sep each with comma and one space.")
         bot.register_next_step_handler(message, set_user_name)
-    else: 
+    else:
         bot.reply_to(message, "Invalid input.\nPlease enter number of user again.")
         bot.register_next_step_handler(message, get_num_of_people)
 
@@ -62,11 +62,33 @@ def transaction_process(message):
             table.at[sender, receiver] += amount
             table.to_csv("user_table.csv", index = True)
             bot.reply_to(message, f"Transaction of {amount} from {sender} to {receiver} recorded.")
-        else: 
+        else:
             bot.reply_to(message, "Invalid sender or receiver. Please enter the transaction again.")
             bot.register_next_step_handler(message, enter_transaction)
 
-        
+@bot.message_handler(commands=['bal'])
+def bal_query(message):
+    bot.reply_to(message, "Enter a pair of user to look up balance: [user1] [user 2]")
+    bot.register_next_step_handler(message, show_bal)
+
+def show_bal(message):
+    user = message.text.split()
+    user1 = user[0]
+    user2 = user[1]
+    table = pd.read_csv("user_table.csv", index_col = 0)
+    if not user1 == user2 and user1 in table.index and user2 in table.index:
+        m12 = table.at[user1, user2]
+        m21 = table.at[user2, user1]
+        if(m12 >= m21):
+            bot.reply_to(message, f"*--- Balance of {user1} and {user2} ---*\n{user2} should give {user1} {m12-m21}")
+        else:
+            bot.reply_to(message, f"*--- Balance of {user1} and {user2} ---*\n{user1} should give {user2} {m21-m12}")
+    else:
+        bot.reply_to(message, "Invalid query. Please enter users again")
+        bot.register_next_step_handler(message, bal_query)
+
+
+
 
 
 
